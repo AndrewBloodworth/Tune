a28d6668b2b1041dd3fc38bfcf1e2c011cb0bf65
AndrewBloodworth <andrewcbld@gmail.com>
2021-09-20 23:03:27 -0600

    Spotifysdk is not working

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,12 @@
-#import "GeneratedPluginRegistrant.h"
+//
+//  Runner-Bridging-Header.h
+//  Runner
+//
+//  Created by Andrew Bloodworth on 9/20/21.
+//
+
+#ifndef Runner_Bridging_Header_h
+#define Runner_Bridging_Header_h
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* Runner_Bridging_Header_h */
